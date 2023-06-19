fb948c9842fc1d483b4ab9e435d47275dd1cb9d1
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 12:28:58 -0600

    demo: Lesson 4 part 1

--- demo/app/src/main/java/dev/goobar/advancedandroiddemo/details/AndroidVersionsDetailsScreen.kt
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/details/AndroidVersionsDetailsScreen.kt
@@ -1,28 +1,62 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package dev.goobar.advancedandroiddemo.details
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import dev.goobar.advancedandroiddemo.data.AndroidVersionInfo
+import dev.goobar.advancedandroiddemo.R
+import dev.goobar.advancedandroiddemo.data.AndroidVersionsRepository
 
 @Composable
-internal fun AndroidVersionDetailsScreen(info: AndroidVersionInfo, onBack: () -> Unit) {
-    BackHandler(enabled = true, onBack = onBack)
-    Column(modifier = Modifier.padding(20.dp)) {
-        Text(text = info.publicName, style = MaterialTheme.typography.displayMedium)
-        Text(text = "${info.codename} - API ${info.apiVersion}", style = MaterialTheme.typography.headlineSmall)
-        Text(
-            modifier = Modifier.padding(top = 20.dp),
-            text = info.details,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis
-        )
+internal fun AndroidVersionDetailsScreen(apiVersion: Int, onBack: () -> Unit) {
+    val info = AndroidVersionsRepository.data.first { it.apiVersion == apiVersion }
 
+    BackHandler(enabled = true, onBack = onBack)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = info.publicName, modifier = Modifier.padding(start = 20.dp)) },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_arrow_back),
+                            contentDescription = "Back arrow"
+                        )
+                    }
+                }
+            )
+
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            Column(modifier = Modifier.padding(20.dp)) {
+                Text(text = info.publicName, style = MaterialTheme.typography.displayMedium)
+                Text(
+                    text = "${info.codename} - API ${info.apiVersion}",
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    modifier = Modifier.padding(top = 20.dp),
+                    text = info.details,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
     }
 }
