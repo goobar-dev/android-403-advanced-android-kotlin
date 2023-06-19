488e8c284f74346a4b84c79ee2ed2df5e74988ab
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 12:04:32 -0600

    demo: Lesson 2

--- demo/app/src/main/java/dev/goobar/advancedandroiddemo/ui/theme/Color.kt
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package dev.goobar.advancedandroiddemo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue = Color(0xFF345da8)
+val Sage = Color(0xFF738e8a)
+val Sand = Color(0xFFa38f5b)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlueDark = Color(0xFFaec6ff)
+val SageDark = Color(0xFF002e6b)
+val SandDark = Color(0xFF12448f)
