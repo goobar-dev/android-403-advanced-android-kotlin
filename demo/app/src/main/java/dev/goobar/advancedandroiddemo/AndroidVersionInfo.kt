fcfac5e499d904b84c59e9a699364a06c0f2d601
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 12:10:55 -0600

    demo: Lesson 3

--- demo/app/src/main/java/dev/goobar/advancedandroiddemo/AndroidVersionInfo.kt
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/AndroidVersionInfo.kt
@@ -1,8 +1,15 @@
 package dev.goobar.advancedandroiddemo
 
+import android.os.Parcelable
+import androidx.versionedparcelable.VersionedParcelable
+import androidx.versionedparcelable.VersionedParcelize
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@VersionedParcelize
 data class AndroidVersionInfo(
     val apiVersion: Int,
     val publicName: String,
     val codename: String,
     val details: String
-)
+): VersionedParcelable, Parcelable
