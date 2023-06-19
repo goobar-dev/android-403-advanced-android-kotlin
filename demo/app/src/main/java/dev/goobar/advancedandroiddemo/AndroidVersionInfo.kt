67c268dea129fe3bb0400ee5ee9cb463651eb5f7
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 12:00:40 -0600

    setup: Lesson 2 starter code

---
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/AndroidVersionInfo.kt
@@ -0,0 +1,8 @@
+package dev.goobar.advancedandroiddemo
+
+data class AndroidVersionInfo(
+    val apiVersion: Int,
+    val publicName: String,
+    val codename: String,
+    val details: String
+)
