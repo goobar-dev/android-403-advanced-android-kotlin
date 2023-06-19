dea94e24a8fd14283f39f229a43e5fbf0a5eceee
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 13:21:40 -0600

    demo: Lesson 5 part 3

--- demo/app/src/main/java/dev/goobar/advancedandroiddemo/versions/AndroidVersionsListViewModel.kt
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/versions/AndroidVersionsListViewModel.kt
@@ -1,24 +1,47 @@
 package dev.goobar.advancedandroiddemo.versions
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.goobar.advancedandroiddemo.data.AndroidVersionInfo
 import dev.goobar.advancedandroiddemo.data.AndroidVersionsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+enum class Sort {
+    ASCENDING, DESCENDING
+}
 
 class AndroidVersionsListViewModel : ViewModel() {
 
-    val state: StateFlow<AndroidVersionsListViewModel.State> =
-        MutableStateFlow(
-            State(AndroidVersionsRepository.data.map { info ->
-                State.AndroidVersionViewItem(
-                    title = info.publicName,
-                    subtitle = "${info.codename} - API ${info.apiVersion}",
-                    description = info.details,
-                    info = info
-                )
-            })
-        )
+    private val _sort: MutableStateFlow<Sort> = MutableStateFlow(Sort.ASCENDING)
+    private val _versions: MutableStateFlow<List<AndroidVersionInfo>> = MutableStateFlow(AndroidVersionsRepository.data)
+
+    val state = combine(_sort, _versions) { sort, versions ->
+        val sortedItems = when (sort) {
+            Sort.ASCENDING -> versions.sortedBy { it.apiVersion }
+            Sort.DESCENDING -> versions.sortedByDescending { it.apiVersion }
+        }.map { info ->
+            State.AndroidVersionViewItem(
+                title = info.publicName,
+                subtitle = "${info.codename} - API ${info.apiVersion}",
+                description = info.details,
+                info = info
+            )
+        }
+        State(sortedItems)
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(3_000), State(emptyList()))
+
+    fun onSortClicked() {
+        _sort.update { currentSort ->
+            when (currentSort) {
+                Sort.ASCENDING -> Sort.DESCENDING
+                Sort.DESCENDING -> Sort.ASCENDING
+            }
+        }
+    }
 
     data class State(val versionsList: List<AndroidVersionViewItem>) {
         data class AndroidVersionViewItem(
