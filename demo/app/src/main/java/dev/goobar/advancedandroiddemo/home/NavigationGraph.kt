4cb32a232f94630a89d37ba7be758bf58a06e584
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 12:45:57 -0600

    demo: Lesson 4 part 3

--- demo/app/src/main/java/dev/goobar/advancedandroiddemo/home/NavigationGraph.kt
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/home/NavigationGraph.kt
@@ -1,23 +1,50 @@
+@file:OptIn(ExperimentalAnimationApi::class)
+
 package dev.goobar.advancedandroiddemo.home
 
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dev.goobar.advancedandroiddemo.details.AndroidVersionDetailsScreen
 import dev.goobar.advancedandroiddemo.versions.AndroidVersionsListScreen
 
 @Composable
 internal fun DemoNavigationGraph() {
-    val navController = rememberNavController()
+    val navController = rememberAnimatedNavController()
 
-    NavHost(navController = navController, startDestination = DemoNavigationDestinations.VersionsList.route) {
-        composable(DemoNavigationDestinations.VersionsList.route) {
+    AnimatedNavHost(
+        navController = navController,
+        startDestination = DemoNavigationDestinations.VersionsList.route
+    ) {
+        composable(route = DemoNavigationDestinations.VersionsList.route) {
             AndroidVersionsListScreen { info ->
-                navController.navigate(DemoNavigationDestinations.VersionDetails.createRouteWithArgs(info))
+                navController.navigate(
+                    DemoNavigationDestinations.VersionDetails.createRouteWithArgs(
+                        info
+                    )
+                )
             }
         }
-        composable(route = DemoNavigationDestinations.VersionDetails.route) { entry ->
+        composable(
+            route = DemoNavigationDestinations.VersionDetails.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentScope.SlideDirection.Left,
+                    animationSpec = tween(durationMillis = 300, easing = FastOutLinearInEasing)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentScope.SlideDirection.Right,
+                    animationSpec = tween(durationMillis = 300, easing = FastOutLinearInEasing)
+                )
+            },
+        ) { entry ->
             val info = DemoNavigationDestinations.VersionDetails.retrieveArgs(entry.arguments)
             AndroidVersionDetailsScreen(info) {
                 navController.popBackStack()
