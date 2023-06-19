57acbb827113253474196ea76f93309e3d000722
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 12:00:40 -0600

    setup: Lesson 2 starter code

---
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/AndroidVersionRepository.kt
@@ -0,0 +1,74 @@
+package dev.goobar.advancedandroiddemo
+
+object AndroidVersionsRepository {
+
+    val data = listOf(
+        AndroidVersionInfo(
+            apiVersion = 1,
+            publicName = "Android 1.0",
+            codename = "(no codename)",
+            details = "The initial Android OS release"
+        ),
+        AndroidVersionInfo(
+            apiVersion = 2,
+            publicName = "Android 1.1",
+            codename = "(no codename)",
+            details = ""
+        ),
+        AndroidVersionInfo(
+            apiVersion = 3,
+            publicName = "Android 1.5",
+            codename = "Cupcake",
+            details = "The first version to include the popular dessert code names"
+        ),
+        AndroidVersionInfo(
+            apiVersion = 4,
+            publicName = "Android 1.6",
+            codename = "Donut",
+            details = ""
+        ),
+        AndroidVersionInfo(
+            apiVersion = 26,
+            publicName = "Android 8.0",
+            codename = "Oreo",
+            details = ""
+        ),
+        AndroidVersionInfo(
+            apiVersion = 27,
+            publicName = "Android 8.1",
+            codename = "Oreo",
+            details = ""
+        ),
+        AndroidVersionInfo(
+            apiVersion = 28,
+            publicName = "Android 9.0",
+            codename = "Pie",
+            details = "The last Android OS version to include an official dessert code name"
+        ),
+        AndroidVersionInfo(
+            apiVersion = 29,
+            publicName = "Android 10.0",
+            codename = "Android10",
+            details = "The first mature Android OS version to drop the dessert code names"
+        ),
+        AndroidVersionInfo(
+            apiVersion = 30,
+            publicName = "Android 11",
+            codename = "Android11",
+            details = ""
+        ),
+        AndroidVersionInfo(
+            apiVersion = 31,
+            publicName = "Android 12",
+            codename = "Android12",
+            details = ""
+        ),
+        AndroidVersionInfo(
+            apiVersion = 32,
+            publicName = "Android 12L",
+            codename = "Android 12L",
+            details = ""
+        ),
+    )
+
+}
