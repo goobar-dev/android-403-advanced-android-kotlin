d2f3edccb72e3050a4176c6acf2aa03195f6e7ce
Nate Ebel <n8ebel@goobar.dev>
2023-06-19 23:47:47 -0600

    demo: Lesson 11 part 2

--- demo/app/src/main/java/dev/goobar/advancedandroiddemo/di/DatabaseModule.kt
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/di/DatabaseModule.kt
@@ -1,14 +1,17 @@
 package dev.goobar.advancedandroiddemo.di
 
 import android.content.Context
+import net.sqlcipher.database.SQLiteDatabase
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import dev.goobar.advancedandroiddemo.BuildConfig
 import dev.goobar.advancedandroiddemo.db.AppDatabase
 import dev.goobar.advancedandroiddemo.db.NoteDao
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -22,10 +25,12 @@ class DatabaseModule {
     @Provides
     @Singleton
     fun provideAppDatabase(@ApplicationContext appContext: Context): AppDatabase {
+        val supportFactory =
+            SupportFactory(SQLiteDatabase.getBytes("password".toCharArray()))
         return Room.databaseBuilder(
             appContext,
             AppDatabase::class.java,
             "android-study-guide-database"
-        ).build()
+        ).openHelperFactory(supportFactory).build()
     }
 }
