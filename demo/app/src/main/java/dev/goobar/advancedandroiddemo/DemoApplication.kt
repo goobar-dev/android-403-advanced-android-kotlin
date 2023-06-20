1c89448907ee4a5d6c397ef774ca8eff6b26e47f
Nate Ebel <n8ebel@goobar.dev>
2023-06-20 00:31:11 -0600

    demo: Lesson 13

--- demo/app/src/main/java/dev/goobar/advancedandroiddemo/DemoApplication.kt
+++ demo/app/src/main/java/dev/goobar/advancedandroiddemo/DemoApplication.kt
@@ -1,7 +1,34 @@
 package dev.goobar.advancedandroiddemo
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
 import dagger.hilt.android.HiltAndroidApp
+import dev.goobar.advancedandroiddemo.sync.FeaturedReposSyncWorker
+import javax.inject.Inject
 
 @HiltAndroidApp
-class DemoApplication : Application()
+class DemoApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val uploadWorkRequest: WorkRequest =
+            OneTimeWorkRequestBuilder<FeaturedReposSyncWorker>()
+                .build()
+
+        WorkManager
+            .getInstance(this)
+            .enqueue(uploadWorkRequest)
+    }
+}
